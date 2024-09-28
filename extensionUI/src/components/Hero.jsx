6c79faecd254d61151d3c5abf7332d51962aa225
Divine-coder250 <lincadivine@gmail.com>
2024-09-28 12:05:05 +0200

    some changes

--- extensionUI/src/components/Hero.jsx
+++ extensionUI/src/components/Hero.jsx
@@ -4,14 +4,13 @@ const Hero = () => {
   return (
     <section className="text-center p-10 bg-dark text-white">
       <h1 className="text-4xl text-black font-bold">SAFEGUARD YOUR ONLINE EXPERIENCE WITH OUR TRUSTARMOR AI!</h1>
-      <p className="mt-4 text-gray-300 text-lg ">
-      TrustArmor AI enhances your online safety by detecting fraudulent links and verifying image sources. Stay protected from scams and misinformation while browsing, ensuring a secure and trustworthy internet experience.
+      <p className="mt-4 text-gray-500 text-lg">
+        TrustArmor AI enhances your online safety by detecting fraudulent links and verifying image sources. Stay protected from scams and misinformation while browsing, ensuring a secure and trustworthy internet experience.
       </p>
       <div className="mt-6 flex flex-col space-y-4 md:flex-row md:space-x-4 md:space-y-0 justify-center">
-        <button className="bg-purple-600 rounded text-white flex-1 h-12 md:h-14 flex items-center justify-center px-6">
+        <button className="bg-purple-600 rounded text-white flex items-center justify-center px-8 w-48 h-12 md:h-14">
           Book A Demo
         </button>
-      
       </div>
     </section>
   );
